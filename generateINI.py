e433322817c32348dff246cb0efe9ac9ceb13d60
John Allwine <jallwine86@gmail.com>
2019-01-09 19:01:37 +0000

    first feature overlays for new bbb image
    SOFT-130

--- generateINI.py
+++ generateINI.py
@@ -16,16 +16,32 @@ from ini import read_ini_data, merge_ini_data, write_ini_data
 INI_FILE = os.path.join(POCKETNC_DIRECTORY, "Settings/PocketNC.ini")
 INI_DEFAULT_FILE = os.path.join(POCKETNC_DIRECTORY, "Settings/versions/%s/PocketNC.ini" % VERSION)
 CALIBRATION_OVERLAY_FILE = os.path.join(POCKETNC_DIRECTORY, "Settings/CalibrationOverlay.inc")
+FEATURES_DIR = os.path.join(POCKETNC_DIRECTORY, "Settings/features")
 
 if __name__ == "__main__":
-    defaults = read_ini_data(INI_DEFAULT_FILE)
+  defaults = read_ini_data(INI_DEFAULT_FILE)
 
-    if os.path.isfile(CALIBRATION_OVERLAY_FILE):
-        overlay = read_ini_data(CALIBRATION_OVERLAY_FILE)
-    else:
-        overlay = { 'parameters': [],
-                    'sections': {} }
+  if os.path.isfile(CALIBRATION_OVERLAY_FILE):
+    overlay = read_ini_data(CALIBRATION_OVERLAY_FILE)
+  else:
+    overlay = { 'parameters': [],
+                'sections': {} }
 
-    merged = merge_ini_data(defaults, overlay)
+  merged = merge_ini_data(defaults, overlay)
 
-    write_ini_data(merged, INI_FILE);
+  for param in overlay['parameters']:
+    section = param['values']['section']
+    name = param['values']['name']
+    value = param['values']['value']
+
+    if section == "POCKETNC_FEATURES":
+      dir = os.path.join(FEATURES_DIR, name.lower())
+
+      feature_overlay_path = os.path.join(dir, "overlay.inc")
+
+      if os.path.isfile(feature_overlay_path):
+          feature_overlay = read_ini_data(feature_overlay_path)
+          merged = merge_ini_data(merged, feature_overlay)
+    
+
+  write_ini_data(merged, INI_FILE);
