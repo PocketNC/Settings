9053feb51e96c20afeef5fcfb976caf25f426bd6
John Allwine <john@pocketnc.com>
2019-01-11 17:06:23 +0000

    added automatic detection of features using a detect script
    * place a detect script in the feature directory that prints 1
      if the necessary requirements are detected or 0 otherwise
    * the generateINI.py script will include the feature if detected
    * the CalibrationOverlay.inc can override the detected feature
      by setting the parameter with the same name, but all uppercase to 0
      under the POCKETNC_FEATURES section
    
    SOFT-130
    SOFT-62

--- generateINI.py
+++ generateINI.py
@@ -30,27 +30,49 @@ if __name__ == "__main__":
 
   merged = merge_ini_data(defaults, overlay)
 
+  features = set()
+
+  # Auto detected features
+  for feature in os.listdir(FEATURES_DIR):
+    feature_path = os.path.join(FEATURES_DIR, feature)
+    detect_path = os.path.join(feature_path, "detect")
+
+    if os.path.isfile(detect_path):
+      detected = subprocess.check_output(detect_path).strip()
+      if detected == "1":
+        print "Detected feature, %s" % feature
+        features.add(feature)
+
+  # Manually enabled/disabled features
   for param in overlay['parameters']:
     section = param['values']['section']
     name = param['values']['name']
     value = param['values']['value']
 
     if section == "POCKETNC_FEATURES":
-      dir = os.path.join(FEATURES_DIR, name.lower())
+      feature = name.lower()
+      if value == "1":
+        features.add(feature)
+      elif value == "0" and feature in features:
+        features.remove(feature)
+        
 
-      feature_overlay_path = os.path.join(dir, "overlay.inc")
-      feature_append_path = os.path.join(dir, "append.inc")
-      feature_startup_path = os.path.join(dir, "startup.sh")
+  for feature in features:
+    dir = os.path.join(FEATURES_DIR, feature)
 
-      if os.path.isfile(feature_overlay_path):
-        feature_overlay = read_ini_data(feature_overlay_path)
-        merged = merge_ini_data(merged, feature_overlay)
+    feature_overlay_path = os.path.join(dir, "overlay.inc")
+    feature_append_path = os.path.join(dir, "append.inc")
+    feature_startup_path = os.path.join(dir, "startup")
 
-      if os.path.isfile(feature_append_path):
-        feature_append = read_ini_data(feature_append_path)
-        merged = append_ini_data(merged, feature_append)
+    if os.path.isfile(feature_overlay_path):
+      feature_overlay = read_ini_data(feature_overlay_path)
+      merged = merge_ini_data(merged, feature_overlay)
 
-      if os.path.isfile(feature_startup_path):
-        subprocess.check_output(feature_startup_path);
+    if os.path.isfile(feature_append_path):
+      feature_append = read_ini_data(feature_append_path)
+      merged = append_ini_data(merged, feature_append)
+
+    if os.path.isfile(feature_startup_path):
+      subprocess.check_output(feature_startup_path);
 
   write_ini_data(merged, INI_FILE);
