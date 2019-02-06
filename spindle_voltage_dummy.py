304b44b01c772364fd096abcdc30aaf1908fde41
John Allwine <jallwine86@gmail.com>
2019-02-06 21:43:42 +0000

    properly configure dummy spindle_voltage component SOFT-64

--- spindle_voltage_dummy.py
+++ spindle_voltage_dummy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import hal
+import time
 
 h = hal.component("spindle_voltage")
 h.newpin("speed_in", hal.HAL_FLOAT, hal.HAL_IN)
@@ -8,3 +9,9 @@ h.newpin("speed_measured", hal.HAL_FLOAT, hal.HAL_OUT)
 h.ready()
 
 h['speed_measured'] = 0
+
+try:
+  while True:
+    time.sleep(100000)
+except KeyboardInterrupt:
+  raise SystemExit
