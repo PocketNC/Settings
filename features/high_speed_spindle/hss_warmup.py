3352f9b75e0a79e00dba1ce9dbeff7805e7dce4c
John Allwine <jallwine86@gmail.com>
2019-02-12 13:46:18 +0000

    only log spindle off time when not aborted invert spindle enable pin

--- features/high_speed_spindle/hss_warmup.py
+++ features/high_speed_spindle/hss_warmup.py
@@ -14,9 +14,16 @@ h = hal.component("hss_warmup")
 h.newpin("spindle_on", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("program_running", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("performing_warmup", hal.HAL_BIT, hal.HAL_IO)
+
+# set to true when an E Stop should occur
 h.newpin("abort", hal.HAL_BIT, hal.HAL_OUT)
 
+# set to true when aborted and reset by rockhopper after
+# the error has been reported
+h.newpin("aborted", hal.HAL_BIT, hal.HAL_OUT)
+
 h['abort'] = False
+h['aborted'] = False
 
 h.ready()
 
@@ -38,24 +45,32 @@ def checkSpindleActivity():
   return not h['performing_warmup'] and totalTime > 10
 
 lastSpindleOn = False
+abort = False
 
 try:
   while True:
-    if lastSpindleOn and not h['spindle_on'] and not h['performing_warmup']:
+    if lastSpindleOn and not h['spindle_on'] and not h['performing_warmup'] and not abort:
+      # if we turned the spindle off, record the current time
+      # unless we cancelled the warm up sequence or someone tried to turn on the
+      # spindle without warming up
       print "recording last time spindle was turned off"
       f = open(LAST_SPINDLE_OFF_FILE, 'w');
       f.write(datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ\n"))
       f.close()
 
     if h['spindle_on'] and not lastSpindleOn:
+      # if the spindle is being turned on, check whether we should abort
       abort = checkSpindleActivity()
       if abort:
-        print "Aborting!"
-      else:
-        print "not aborting..."
-      h['abort'] = abort
+        h['aborted'] = True
+    else:
+      abort = False
 
+    h['abort'] = abort
     if not h['program_running']:
+      # if the warm up sequence is canceled, then the performing_warmup
+      # pin won't properly be turned off, so if a program isn't running
+      # turn it off here
       h['performing_warmup'] = False
 
     lastSpindleOn = h['spindle_on']
