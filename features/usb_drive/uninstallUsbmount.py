e6e95405eb98fffa0b6e47902ee4b8a17466b648
Jake Danczyk <jake@pocketnc.com>
2020-01-29 17:40:23 +0000

    SOFT-6 Moved script for uninstalling usbmount into Settings/features/usb_drive.

---
+++ features/usb_drive/uninstallUsbmount.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python 
+
+import subprocess
+import sys
+import re
+
+# In Pocket NC software version 4.3.0 we added the ability to run GCODE stored on a USB drive. 
+# This depends on a Debian package called usbmount to detect a USB drive and automount it to the filesystem. 
+# If a user needs to roll back their software version to something earlier, then usbmount needs to be removed
+
+v = sys.argv[1]
+
+def toIntOrString(text):
+    try:
+        retval = int(text)
+    except ValueError:
+        retval = text
+    return retval
+
+def natural_keys(text):
+    return [ toIntOrString(c) for c in re.split('[v.-]', text) ]
+
+
+if natural_keys(v) < natural_keys("v4.3.0"):
+    removalCommand = "sudo apt-get --purge -y remove usbmount"    
+    p = subprocess.Popen(removalCommand.split(), stderr=subprocess.PIPE, stdout=subprocess.PIPE )
+    
