50055d0da1e47d659c9e3a23f5db56f8eb621b10
Jake Danczyk <jake@pocketnc.com>
2020-02-19 20:10:10 +0000

    Missing usbmount dependencies for boards runing kernel 4.19 SOFT-6

--- features/usb_drive/uninstallUsbmount.py
+++ features/usb_drive/uninstallUsbmount.py
@@ -27,6 +27,6 @@ if natural_keys(v) < natural_keys("v4.3.0"):
   kernelCheckCommand = "uname -r"
   p = subprocess.Popen( kernelCheckCommand.split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE )
   if '4.19.59' in p.stdout.read():
-    removalCommand = "sudo apt-get --purge -y remove lockfile-progs"
+    removalCommand = "sudo apt-get --purge -y remove lockfile-progs liblockfile1 liblockfile-bin"
     p = subprocess.Popen( removalCommand.split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE )
     p.wait()
