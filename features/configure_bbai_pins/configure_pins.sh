55629f8692b0cfdeb099777d204c5a814b8cb14c
John Allwine <john@pocketnc.com>
2020-05-06 16:12:21 +0000

    renamed labels to match GPIO ports on the ai

--- features/configure_bbai_pins/configure_pins.sh
+++ features/configure_bbai_pins/configure_pins.sh
@@ -46,33 +46,33 @@ while read PIN DIR JUNK ; do
         esac
 
 done <<- EOF
-	166	in	# P8.8		gpio2.3		Spindle Clock
-	178	in	# P8.9		gpio2.5		X Limit
-	164	in	# P8.10		gpio2.4		E-Stop Signal
-	75	out	# P8.11		gpio1.13	B Dir
-	74	out	# P8.12		gpio1.12	B Step
-	107	out	# P8.13		gpio0.23	Spindle PWM
-	109	in	# P8.14		gpio0.26	Y Limit
-	99	out	# P8.15		gpio1.15	Z Dir
-	125	out	# P8.16		gpio1.14	Z Step
-	242	out	# P8.17		gpio0.27	E-Stop LED
-	105	in	# P8.18		gpio2.1		Z Limit
-	106	in	# P8.19		gpio0.22	B Limit
-	124	in	# p8.26		gpio1.29	Start Signal
-	241	out	# p9.11		gpio0.30	Spindle Dir
-	128	out	# p9.12		gpio1.28	Enable
-	121	out	# p9.14		gpio1.18	Start LED
-	76	out	# p9.15		gpio1.16	Y Dir
-	122	in	# p9.16		gpio1.19	A Limit
-	209	in	# p9.17		gpio0.5		Unused
-	67	in	# p9.21		gpio0.3		Interlock
-	179	in	# p9.22		gpio0.2		Probe Signal
-	203	out	# p9.23		gpio1.17	Y Step
-	175	out	# P9.24		gpio0.15	Spindle On
-	111	out	# P9.27		gpio3.19	X Step
-	113	out	# P9.28		gpio3.17	A Dir
-	139	out	# p9.29		gpio3.15        X Dir
-	140	out	# p9.30		gpio3.16	A Step
+	166	in	# P8.8		gpio6.6		Spindle Clock
+	178	in	# P8.9		gpio6.18	X Limit
+	164	in	# P8.10		gpio6.4		E-Stop Signal
+	75	out	# P8.11		gpio3.11	B Dir
+	74	out	# P8.12		gpio3.10	B Step
+	107	out	# P8.13		gpio4.11	Spindle PWM
+	109	in	# P8.14		gpio4.13	Y Limit
+	99	out	# P8.15		gpio4.3 	Z Dir
+	125	out	# P8.16		gpio4.29	Z Step
+	242	out	# P8.17		gpio8.18	E-Stop LED
+	105	in	# P8.18		gpio4.9		Z Limit
+	106	in	# P8.19		gpio4.10	B Limit
+	124	in	# p8.26		gpio4.28	Start Signal
+	241	out	# p9.11		gpio8.17	Spindle Dir
+	128	out	# p9.12		gpio5.0 	Enable
+	121	out	# p9.14		gpio4.25	Start LED
+	76	out	# p9.15		gpio3.12	Y Dir
+	122	in	# p9.16		gpio4.26	A Limit
+	209	in	# p9.17		gpio7.17	Unused
+	67	in	# p9.21		gpio3.3		Interlock
+	179	in	# p9.22		gpio6.19	Probe Signal
+	203	out	# p9.23		gpio7.11	Y Step
+	175	out	# P9.24		gpio6.15	Spindle On
+	111	out	# P9.27		gpio4.15	X Step
+	113	out	# P9.28		gpio4.17	A Dir
+	139	out	# p9.29		gpio5.11        X Dir
+	140	out	# p9.30		gpio5.12	A Step
 EOF
 
 sudo -A su -c "echo rising > /sys/class/gpio/gpio67/edge" || interrupt_err # set interrupt on p8.8 
