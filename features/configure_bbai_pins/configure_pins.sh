de799d23c3e46c16112a4360e8db52e91944f6e9
John Allwine <john@pocketnc.com>
2020-10-14 22:07:01 +0000

    pro 1.0.0 pin changes, prototype machine 2 config updates SOFT-525

--- features/configure_bbai_pins/configure_pins.sh
+++ features/configure_bbai_pins/configure_pins.sh
@@ -46,44 +46,32 @@ while read PIN DIR JUNK ; do
         esac
 
 done <<- EOF
-	165	in	# P8.7		gpio6.5		B Feedback
-	166	in	# P8.8		gpio6.6		Spindle Clock
 	178	in	# P8.9		gpio6.18	X Limit
-	164	in	# P8.10		gpio6.4		E-Stop Signal
-	75	in	# P8.11		gpio3.11	B Dir - V2
-	74	in	# P8.12		gpio3.10	B Step - V2
-	107	out	# P8.13		gpio4.11	Spindle PWM
+	164	in	# P8.10		gpio6.4		E-Stop 5V Connected
+	107	in	# P8.13		gpio4.11	E-Stop GND Connected
 	109	in	# P8.14		gpio4.13	Y Limit
-	99	in	# P8.15		gpio4.3 	Z Dir - V2
-	125	in	# P8.16		gpio4.29	Z Step - V2
 	242	out	# P8.17		gpio8.18	E-Stop LED
-	105	in	# P8.18		gpio4.9		Z Limit
-	106	in	# P8.19		gpio4.10	B Limit
 	 22	out	# P8.23		gpio1.22	X Enable
+	192	in	# P8.24		gpio7.0 	I2C Expander Interrupt 1A
+	191	in	# P8.25		gpio6.31 	I2C Expander Interrupt 2A
 	124	in	# p8.26		gpio4.28	Start Signal
-	119	in	# p8.27		gpio8.19	C Feedback
-	118	in	# p8.30		gpio4.20	Y Feedback
+	119	in	# p8.27		gpio8.19	Tool Changer Open
+	115	in	# p8.28		gpio4.19	Tool Changer Closed
+	118	in	# p8.29		gpio4.22	Tool Changer Home
 	238	out	# p8.31		gpio8.14	Y Enable
+	239	out	# p8.32		gpio8.15	Tool Changer Enable
 	236	out	# p8.35		gpio8.12	Z Enable
 	230	out	# p8.39		gpio8.6	        B Enable
 	226	out	# p8.43		gpio8.2	        C Enable
-	241	out	# p9.11		gpio8.17	Spindle Dir
 	128	out	# p9.12		gpio5.0 	Enable
-	172	in	# p9.13		gpio6.12	Y Feedback - not connected
 	121	out	# p9.14		gpio4.25	Start LED
-	76	in	# p9.15		gpio3.12	Y Dir - V2
-	122	in	# p9.16		gpio4.26	A Limit
-	209	in	# p9.17		gpio7.17	Unused
-	67	in	# p9.21		gpio3.3		Interlock
-	179	in	# p9.22		gpio6.19	Probe Signal
-	203	in	# p9.23		gpio7.11	Y Step - V2
-	175	out	# P9.24		gpio6.15	Spindle On
-	177	in	# P9.25		gpio6.17	X Feedback
-	174	in	# P9.26		gpio6.14	Z Feedback
-	111	in	# P9.27		gpio4.15	X Step - V2
-	113	in	# P9.28		gpio4.17	A Dir - V2
-	139	in	# p9.29		gpio5.11        X Dir - V2
-	140	in	# p9.30		gpio5.12	A Step - V2
+	122	in	# p9.16		gpio4.26	B Limit
+	203	in	# p9.23		gpio7.11	Interlock 2
+	175	in	# P9.24		gpio6.15	Probe 1 Signal
+	177	in	# P9.25		gpio6.17	Probe 2 Signal
+	111	in	# P9.27		gpio4.15	Z Limit
+	113	in	# P9.28		gpio4.17	C Limit
+	139	in	# p9.29		gpio5.11        Interlock
 EOF
 
-sudo -A su -c "echo rising > /sys/class/gpio/gpio67/edge" || interrupt_err # set interrupt on p8.8 
+#sudo -A su -c "echo rising > /sys/class/gpio/gpio67/edge" || interrupt_err # set interrupt on p8.8 
