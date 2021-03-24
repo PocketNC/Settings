9a33f5bd61bbef570dfe9b87679c6a77c0717a50
John Allwine <john@pocketnc.com>
2021-03-24 20:30:34 +0000

    added I2C object in i2c.py which is a thin wrapper around Adafruit_GPIO.I2C.Device as it will make adapting code that uses Adafruit_GPIO easier.

--- i2c.py
+++ i2c.py
@@ -1,5 +1,6 @@
 from device import getDeviceModel, DeviceModel
 from smbus import SMBus
+import Adafruit_GPIO.I2C
 
 model = getDeviceModel()
 
@@ -9,3 +10,7 @@ elif model == DeviceModel.BBB:
   busNum = 2
 
 bus = SMBus(busNum)
+
+class I2C(Adafruit_GPIO.I2C.Device):
+  def __init__(self, address):
+    super().__init__(address, busNum)
