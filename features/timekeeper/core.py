a514e368ec1411764f67aff67761d6639185c86d
Jake Danczyk <jake@pocketnc.com>
2021-04-29 00:25:10 +0000

    SOFT-356 code cleanup

--- features/timekeeper/core.py
+++ features/timekeeper/core.py
@@ -42,61 +42,6 @@ def runtime_to_bytes(runtime):
     runtime = runtime // 256
   return arr
 
-def initialize():
-  ee = eeprom.EEPROM()
-  #Read current EEPROM state
-  ee_arr = ee.ReadBytes(START_IDX, (END_IDX + 1) - START_IDX)
-  time.sleep(0.1)
-
-  #Find the current position
-  if ee_arr[0] == 255:
-    #This is a fresh EEPROM. Initialize
-    ee.WriteBytes(START_IDX, [0] + runtime_to_bytes(0))
-    time.sleep(0.1)
-    latestBucketIdx = START_IDX
-  else:
-    startTag = ee_arr[0]
-    for tagIdx in list(range(START_IDX, END_IDX + 1, BUCKET_SIZE)):
-      if(ee_arr[tagIdx - START_IDX] != startTag):
-        #Found the first tag with different value than start tag
-        #The preceeding bucket was the last written before shutdown
-        latestBucketIdx = tagIdx - BUCKET_SIZE
-        break
-    if(latestBucketIdx is None):
-      #End bucket was the last bucket written before shutdown
-      latestBucketIdx = (END_IDX - BUCKET_SIZE) + 1
-
-  runtime = runtimeWritten = bytes_to_runtime( ee.ReadBytes( (latestBucketIdx + TAG_SIZE), DATA_SIZE) )
-  return (ee, latestBucketIdx, runtime)
-
-def loop_step(isTicking, runtime, nowTime, lastTime):
-    if isTicking:
-      runtime += (nowTime - lastTime)
-      
-    if(int(runtime) != runtimeWritten and (nowTime - lastWriteTime) > WRITE_PERIOD):
-      try:
-        bucketIdx = (latestBucketIdx + BUCKET_SIZE) % ((END_IDX + 1) - START_IDX)
-        flag = ee.ReadBytes(bucketIdx,1)[0]
-        time.sleep(0.1)
-        newFlag = 1 if flag == 0 else 0
-        b = runtime_to_bytes(int(runtime))
-        runtimeWritten = int(runtime)
-        #If the write will cross EEPROM page boundary, split it up
-        distFromGap = PAGE_SIZE - (bucketIdx % PAGE_SIZE)
-        if(distFromGap < BUCKET_SIZE):          
-          ee.WriteBytes( bucketIdx, [newFlag] + runtime_to_bytes(runtimeWritten)[0:distFromGap - 1])
-          time.sleep(0.1)
-          ee.WriteBytes( bucketIdx + distFromGap, runtime_to_bytes(runtimeWritten)[ distFromGap - 1: ])
-        else:
-          ee.WriteBytes( bucketIdx, [newFlag] + runtime_to_bytes(runtimeWritten))
-        lastWriteTime = time.time()
-        latestBucketIdx = bucketIdx
-      except IOError as e:
-        print(e)
-
-
-
-
 class EEPROMInterface:
   def isTicking(self):
     return self._isTicking
@@ -127,7 +72,6 @@ class EEPROMInterface:
       if(self.latestBucketIdx is None):
         #End bucket was the last bucket written before shutdown
         self.latestBucketIdx = (END_IDX - BUCKET_SIZE) + 1
-    
 
   def write_next(self, timeVal):
     try:
