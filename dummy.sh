2a20967ee9c2e2da3c61d1a969bce76cab0908c9
John Allwine <john@pocketnc.com>
2022-01-12 11:09:10 -0700

    added RS274NGC_STARTUP_CODES to initialize smoothing parameter removed reset_interpreter.py to preserve RS274NGC_STARTUP_CODEs SOFT-807

--- dummy.sh
+++ dummy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-
-# This python script resets the linuxcnc interpreter so that status values are
-# refetched, this is a workaround to an issue with incorrect initial values for
-# work offset status values: g5x_index, g5x_offset, g92_offset
-# It is run here so that this always occurs during linuxcnc startup
-# but after all other startup steps have finished.
-linuxcnc-python ${POCKETNC_DIRECTORY}/Settings/reset_interpreter.py
-
 echo Started dummy display!
 sleep infinity
--- reset_interpreter.py
+++
@@ -1,9 +0,0 @@
-import linuxcnc
-
-
-# Resetting the linuxcnc interpreter will cause status values to be refetched
-# We are using this to workaround an issue where status items for work offset
-# systems were erroneously always reported as 0 after startup, when behind
-# the scenes the actual effective values are whatever was active at shutdown.
-c = linuxcnc.command()
-c.reset_interpreter()
