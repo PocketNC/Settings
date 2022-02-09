aaf0bd43a74886c7bb102a6ba7450aacf805b3c8
John Allwine <john@pocketnc.com>
2022-02-08 17:13:53 -0700

    added a couple simple probing routines added _f prefix to named parameters that access feature information

--- namedparams.py
+++ namedparams.py
@@ -3,74 +3,78 @@ import math
 
 manager = metrology.FeatureManager.getInstance()
 
-def _average_x(self):
+def _f_points(self):
+  feature = manager.getActiveFeature()
+  return len(feature.points);
+
+def _f_average_x(self):
   feature = manager.getActiveFeature()
   avg = feature.average()
 
   return avg[0]
 
-def _average_y(self):
+def _f_average_y(self):
   feature = manager.getActiveFeature()
   avg = feature.average()
 
   return avg[1]
 
-def _average_z(self):
+def _f_average_z(self):
   feature = manager.getActiveFeature()
   avg = feature.average()
 
   return avg[2]
 
-def _circle2d_center_x(self):
+def _f_circle2d_center_x(self):
   feature = manager.getActiveFeature()
   circle2D = feature.circle2D()
 
   return circle2D[0][0]
 
-def _circle2d_center_y(self):
+def _f_circle2d_center_y(self):
   feature = manager.getActiveFeature()
   circle2D = feature.circle2D()
 
   return circle2D[0][1]
 
-def _circle2d_radius(self):
+def _f_circle2d_radius(self):
   feature = manager.getActiveFeature()
   circle2D = feature.circle2D()
 
   return circle2D[1]
 
-def _circle2d_diameter(self):
+def _f_circle2d_diameter(self):
   feature = manager.getActiveFeature()
   circle2D = feature.circle2D()
 
   return 2*circle2D[1]
 
-def _line_direction_x(self):
+def _f_line_direction_x(self):
   feature = manager.getActiveFeature()
   line = feature.line()
 
   return line[1][0]
 
-def _line_direction_y(self):
+def _f_line_direction_y(self):
   feature = manager.getActiveFeature()
   line = feature.line()
 
   return line[1][1]
 
-def _line_angle_about_x(self):
+def _f_line_angle_about_x(self):
   feature = manager.getActiveFeature()
   line = feature.line()
 
   return math.atan(line[1][1]/(-line[1][2]))
 
-def _line_angle_about_y(self):
+def _f_line_angle_about_y(self):
   feature = manager.getActiveFeature()
   line = feature.line()
 
   return math.atan((-line[1][2])/line[1][0])
 
 
-def _line_angle_about_z(self):
+def _f_line_angle_about_z(self):
   feature = manager.getActiveFeature()
   line = feature.line()
 
