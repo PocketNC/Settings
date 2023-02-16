4da28ebd2dce063e9ecd0f40ee4307e737dfcfa4
John Allwine <john@pocketnc.com>
2023-02-16 15:55:42 -0700

    added _v2_calib_x_offset and _v2_calib_y_offset for offsetting position by our expected home offset error, so we can probe the tool setter button properly

--- namedparams/v2_calib.py
+++ namedparams/v2_calib.py
@@ -1,12 +1,24 @@
 from calibstate import Stages, CalibState
+import v2state
+import v2calculations
 
 def _v2_calib_x_offset(self):
   state = CalibState.getInstance()
-  return state.getHomeOffsetDx(state,self)
+  x_homing_stage = state.getStage(Stages.HOMING_X)
+  probe_offsets_stage = state.getStage(Stages.PROBE_OFFSETS)
+
+  (x_dir,y_dir,z_dir) = v2state.getAxisDirections(state)
+
+  return v2calculations.calc_home_offset_x_error(x_dir, y_dir, x_homing_stage["features"], probe_offsets_stage["x_features"])/25.4
 
 def _v2_calib_y_offset(self):
   state = CalibState.getInstance()
-  return state.getHomeOffsetDy(state,self)
+  x_homing_stage = state.getStage(Stages.HOMING_X)
+  probe_offsets_stage = state.getStage(Stages.PROBE_OFFSETS)
+
+  (x_dir,y_dir,z_dir) = v2state.getAxisDirections(state)
+
+  return v2calculations.calc_home_offset_y_error(x_dir, y_dir, x_homing_stage["features"], probe_offsets_stage["y_features"])/25.4
 
 __all__ = [ k for k in globals().keys() if k.startswith("_v2_calib") ]
 
