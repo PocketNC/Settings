03e9e8c70ed853eefdcfd608e62e460d968347c2
John Allwine <john@pocketnc.com>
2023-02-20 11:30:46 -0700

    reset delay seconds after detecting a change in the pin rather than naively setting the pin pin every .1 seconds SOFT-1038

--- features/penta/reset_pin.py
+++ features/penta/reset_pin.py
@@ -8,11 +8,16 @@ if __name__ == "__main__":
   h = hal.component(sys.argv[1])
   h.newpin("value", hal.HAL_BIT, hal.HAL_IN)
   h.newpin("pin", hal.HAL_BIT, hal.HAL_IO)
+  h.newpin("delay", hal.HAL_FLOAT, hal.HAL_IN)
   h.ready()
+
+  h["delay"] = .1
 
   try:
     while True:
-      h["pin"] = h["value"]
+      if h["pin"] != h["value"]:
+        time.sleep(h["delay"])
+        h["pin"] = h["value"]
       time.sleep(.1)
   except KeyboardInterrupt:
     raise SystemExit
