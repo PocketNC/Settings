168f4c963bf5c3286eb23722e5996fabb8ded862
John Allwine <john@pocketnc.com>
2023-04-05 12:39:12 -0600

    changed getCompensationX, Y and Z to getCompensation()[0], [1] and [2] as the previous merge changed the interface. SOFT-1078

--- namedparams/probe.py
+++ namedparams/probe.py
@@ -2,14 +2,14 @@ import probe
 
 def _probe_compensation_x(self):
   cal = probe.getInstance()
-  return cal.getCompensationX()
+  return cal.getCompensation()[0]
 
 def _probe_compensation_y(self):
   cal = probe.getInstance()
-  return cal.getCompensationY()
+  return cal.getCompensation()[1]
 
 def _probe_compensation_z(self):
   cal = probe.getInstance()
-  return cal.getCompensationZ()
+  return cal.getCompensation()[2]
 
 __all__ = [ k for k in globals().keys() if k.startswith("_probe") ]
