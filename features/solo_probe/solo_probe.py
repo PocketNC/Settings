5d04f0cdc494e286ef9e7b890d48d436f57b2fc3
John Allwine <john@pocketnc.com>
2023-06-23 16:13:45 -0600

    stubbed out a hal component for waking/sleeping probe SOFT-1110

---
+++ features/solo_probe/solo_probe.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+#import IrDA_Probe_I2C
+from i2c import busNum
+import hal
+import time
+
+address = 0x44
+
+#probe = IrDA_Probe_I2C(address, busNum)
+
+h = hal.component("solo-probe")
+
+# pressure is a float from 0-1 that represents a parameter between minimum and maximum pressure
+h.newpin("probe-on", hal.HAL_BIT, hal.HAL_IN)
+
+h.ready()
+
+lastProbeOn = False
+try:
+  while True:
+    try:
+      if h["probe-on"] != lastProbeOn:
+        if h["probe-on"]:
+          # Probe was commanded to turn on
+          # probe.wake()
+          pass
+        else:
+          # Probe was commanded to turn off
+          # probe.sleep()
+          pass
+
+      lastProbeOn = h["probe-on"]
+    except:
+      pass
+
+    time.sleep(.1)
+
+except KeyboardInterrupt:
+  raise SystemExit
+
