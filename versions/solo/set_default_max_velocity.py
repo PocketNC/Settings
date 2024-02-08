c2086cf8d4765d67e78c26e8b21d1c2edd277f63
John Allwine <john@pocketnc.com>
2024-02-08 15:42:32 -0700

    only set default max velocity so rotary axes aren't affected by slower max rapid, and look for [PENTA]DEFAULT_MAX_VELOCITY to do so, so its overridable SOFT-1387

--- versions/solo/set_default_max_velocity.py
+++ versions/solo/set_default_max_velocity.py
@@ -1,14 +1,16 @@
 import linuxcnc
+import ini
+import os
+
+POCKETNC_VAR_DIRECTORY = os.environ.get('POCKETNC_VAR_DIRECTORY')
+INI_FILE_CACHE = ini.read_ini_data(os.path.join(POCKETNC_VAR_DIRECTORY, "PocketNC.ini"))
+
+defaultMaxVelocityParam = ini.get_parameter(INI_FILE_CACHE, 'PENTA', 'DEFAULT_MAX_VELOCITY')
+defaultMaxVelocity = float(defaultMaxVelocityParam['values']['value'] if defaultMaxVelocityParam else 3.33333)
 
 c = linuxcnc.command()
 s = linuxcnc.stat()
 s.poll()
 
-# 2.5 inches/second = 150 inches/minute
-if s.max_velocity > 2.5:
-  c.maxvel(2.5)
-
-defaultRapidRate = 150/590
-
-if s.rapidrate > defaultRapidRate:
-  c.rapidrate(defaultRapidRate)
+if s.max_velocity > defaultMaxVelocity:
+  c.maxvel(defaultMaxVelocity)
